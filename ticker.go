18b6548c156bebd9bf1b57128f58892584c6224a
zhaoguojie2010 <zhaoguojie2010@163.com>
2019-07-11 11:10:49 +0800

    bugfix: fetch timer halt after one attempt

--- ticker.go
+++ ticker.go
@@ -105,7 +105,8 @@ func (t *timeoutTicker) timeoutRoutine() {
 				if newti.Round < ti.Round {
 					continue
 				} else if newti.Round == ti.Round {
-					if ti.Step > 0 && newti.Step <= ti.Step {
+					if ti.Step > 0 && (newti.Step < ti.Step ||
+						newti.Step == ti.Step && newti.Step != RoundStepPrecommitFetch && newti.Step != RoundStepPrevoteFetch) {
 						continue
 					}
 				}
